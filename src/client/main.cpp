e096d28040fc5882a5728063cec91182e5548095
xiaoyu36 <xiaoyu.zhao@ensea.fr>
2022-10-18 16:59:17 +0200

    add the test of state.init

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <algorithm>
+#include <vector>
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
@@ -35,10 +37,70 @@ using namespace state;
 vector<Country> v_listcountry;
 //la liste est definitive, ne touche jamais.
 
-void init_player(Player player1, Player player2, Player player3){
+void listCountries(Player player){
+    cout << "listCountires" << endl;
+    for(unsigned i = 0; i < player.getListCountry().size(); i++){
+        cout << player.getListCountry()[i].getNumberCountry();
+        cout << ' ' << player.getListCountry()[i].getNumberTroop() << endl;
+    }
+}
+
+void jouer(Player player){
+    //choisir les pays attaquant et defensifs
+    int c_attack, c_defender;
+
+    listCountries(player);
+
+    cout << "choose the attack country's number :";
+    cin >>  c_attack;
+
+    cout << "choose the defend country's number :";
+    cin >> c_defender;
+
+//detecter les pays adjacents
+//**************************************//
+/*    if(v_listcountry[c_attack].isAdjacent(c_defender))
+        cout << "isadjacent" << endl;
+    else 
+        cout << "notadjacent" << endl;
+*/
+//**************************************//
+/*    int n_attacker, n_defender;
+
+    cout << "Please set the forces of the attacker :";
+    cin >> n_attacker;
+    v_listcountry[c_attack].addNumberTroop(n_attacker);
+
+    cout << "Please set the forces of the defender :";
+    cin >> n_defender;
+    v_listcountry[c_defender].addNumberTroop(n_defender); */
+//donner les troups aux deux pays 
+
+/*    cout << "attacker's forces :"<< v_listcountry[c_attack].getNumberTroop() << endl;
+    cout << "defender's forces :"<< v_listcountry[c_defender].getNumberTroop() << endl;
+
+    int attack = 1;
+
+    while(attack){
+        attack = player.attack(v_listcountry[c_attack], v_listcountry[c_defender]);
+    }
+
+    cout << v_listcountry[c_attack].getNameCountry() << " have " << v_listcountry[c_attack].getNumberTroop() << " troops" << endl; 
+    cout << v_listcountry[c_defender].getNameCountry() << " have " << v_listcountry[c_defender].getNumberTroop() << " troops" << endl; */
+//*************************************//
+//test pour calculer la probabilte de bataille 
+
+/*  cout << exemple.getX() << endl;
+    cout << player1.getownTroop() << endl;
+    cout << v_listcountry[4].getNameCountry() << v_listcountry[4].getNumberCountry() << endl;
+    cout << player1.getListCountry()[0].getNameCountry() << " " << player1.getListCountry()[0].getNumberCountry() << endl;
+*/
+}
+
+void init_player(Player &player1, Player &player2, Player &player3){
     Dice dice(3,5);
 
-    for(int i = 0; i < 14; i+=3){
+    for(int i = 0; i < 42; i+=3){
         player1.addCountry(v_listcountry[i]);
         v_listcountry[i].addNumberTroop(dice.thrown());
         player2.addCountry(v_listcountry[i+1]);
@@ -53,7 +115,7 @@ void testgame(){
 
     int num_player;
 
-    cout << "choose the number of player :(you need choose 3 for now)";
+    cout << "choose the number of player :(you need choose 3 for now) ";
     cin >> num_player;
 
     if(num_player != 3) return;
@@ -61,6 +123,12 @@ void testgame(){
     Player player1;
     Player player2;
     Player player3;
+
+    vector<Player> player;
+
+    player = state.player;
+
+    cout << "s" << endl;
 
     state.init();
 
@@ -70,62 +138,20 @@ void testgame(){
 //ajouter des pays au joueur1
 //**************************************//
     init_player(player1, player2, player3);
+
+    player.push_back(player1);
+    player.push_back(player2);
+    player.push_back(player3);
+
 //**************************************//
 
 //jouer
-    while(state.playing){
-        
+    while(1){
+        cout << "s" << endl;
+        for(vector<Player>::iterator it = player.begin(); it != player.end(); ++it){
+            jouer(*it);
+        }
     }
-
-
-//choisis les pays attaquant et defensifs
-    int c_attack, c_defender;
-
-    cout << "choose the attack country's number :";
-    cin >>  c_attack;
-
-    cout << "choose the defend country's number :";
-    cin >> c_defender;
-
-//detecter les pays adjacents
-//**************************************//
-    if(v_listcountry[c_attack].isAdjacent(c_defender))
-        cout << "isadjacent" << endl;
-    else 
-        cout << "notadjacent" << endl;
-
-//**************************************//
-    int n_attacker, n_defender;
-
-    cout << "Please set the forces of the attacker :";
-    cin >> n_attacker;
-    v_listcountry[c_attack].addNumberTroop(n_attacker);
-
-    cout << "Please set the forces of the defender :";
-    cin >> n_defender;
-    v_listcountry[c_defender].addNumberTroop(n_defender);
-//donner les troups aux deux pays 
-
-    cout << "attacker's forces :"<< v_listcountry[c_attack].getNumberTroop() << endl;
-    cout << "defender's forces :"<< v_listcountry[c_defender].getNumberTroop() << endl;
-
-    int attack = 1;
-
-    while(attack){
-        attack = player1.attack(v_listcountry[c_attack], v_listcountry[c_defender]);
-    }
-
-    cout << v_listcountry[c_attack].getNameCountry() << " have " << v_listcountry[c_attack].getNumberTroop() << " troops" << endl; 
-    cout << v_listcountry[c_defender].getNameCountry() << " have " << v_listcountry[c_defender].getNumberTroop() << " troops" << endl;
-
-//*************************************//
-//test pour calculer la probabilte de bataille 
-
-/*  cout << exemple.getX() << endl;
-    cout << player1.getownTroop() << endl;
-    cout << v_listcountry[4].getNameCountry() << v_listcountry[4].getNumberCountry() << endl;
-    cout << player1.getListCountry()[0].getNameCountry() << " " << player1.getListCountry()[0].getNumberCountry() << endl;
-*/
 
 }
 
