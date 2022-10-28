b8bf99d62890bca9aa287dbf17da2ad7eeff7a87
Uriel DJENONTIN AGOSSOU <jdjenontin@gmail.com>
2022-10-28 11:18:05 +0200

    unit test

---
+++ test/shared/test_country.cpp
@@ -0,0 +1,22 @@
+
+#include <boost/test/unit_test.hpp>
+
+#include "../../src/shared/state/Country.h"
+
+using namespace ::state;
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+  BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_CASE(TestCountry)
+{
+  {
+    Country country;
+    country.setNumberTroop(5);
+    BOOST_CHECK_EQUAL(country.getNumberTroop(), 2);
+  }
+}
+
+/* vim: set sw=2 sts=2 et : */
