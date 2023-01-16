59da6a229ac548814c6aaaaf9164bab25b112369
Uriel DJENONTIN AGOSSOU <jdjenontin@gmail.com>
2023-01-16 10:10:35 +0100

    temp

--- src/shared/ai/HardAi.cpp
+++ src/shared/ai/HardAi.cpp
@@ -21,6 +21,17 @@ HardAi::HardAi(shared_ptr<State> a_state) : Ai(a_state){
 HardAi::~HardAi(){
 
 }
+
+void HardAi::sortCountryByBSR()
+{
+    std::shared_ptr<state::State> temp = state;
+    
+    auto borderSecurityRatioCmp = [temp](std::shared_ptr<Country>& a, std::shared_ptr<Country>& b) {
+        return Computation::borderSecurityRatio(temp, a) > Computation::borderSecurityRatio(temp, b); 
+    };
+
+    std::sort(countriesList.begin(), countriesList.end(), borderSecurityRatioCmp);
+}
 /**
  * @brief Place Troop in the country with the highest brs
 */
@@ -28,22 +39,27 @@ void HardAi::place(){
     #ifdef DEBUG
         std::cout << "Hard AI :" << __func__ << std::endl;
     #endif
-    std::shared_ptr<state::State> temp = state;
 
     int bonusTroop = player->continentBonusTroop();
 
-    auto borderSecurityRatioCmp = [temp](std::shared_ptr<Country>& a, std::shared_ptr<Country>& b) {
-        return Computation::borderSecurityRatio(temp, a) > Computation::borderSecurityRatio(temp, b); 
-    };
-
     while(bonusTroop>0){
-        std::sort(countriesList.begin(), countriesList.end(), borderSecurityRatioCmp);
+        this->sortCountryByBSR();
         countriesList[0]->addTroop(1);
         bonusTroop--;
     }
 }
 
 void HardAi::attack(){
+    double index = 0;
+    this->sortCountryByBSR();
+
+    std::vector<std::shared_ptr<Country>> attackCandidates;
+    index = countriesList.size()/2;
+
+    attackCandidates.assign(countriesList.begin()+index,countriesList.end());
+    
+
+
 
 }
 
