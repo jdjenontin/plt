a9e7a229669feb085c7960264da271b31a152b20
Uriel DJENONTIN AGOSSOU <jdjenontin@gmail.com>
2023-01-19 16:20:51 +0100

    Minors fixes

--- src/shared/engine/DistributeCard.cpp
+++ src/shared/engine/DistributeCard.cpp
@@ -1,5 +1,4 @@
 #include "DistributeCard.h"
-#include <iostream>
 
 namespace engine {
 
@@ -11,14 +10,10 @@ DistributeCard::~DistributeCard(){
     
 }
 
-//TO-DO improve
 void DistributeCard::execute(){
-    std::vector<std::shared_ptr<state::Card>> cardsList = state->getCardsList();
-    static unsigned int i = 0;
-    if(i<cardsList.size()){
-        player->addCard(cardsList[i]);
-        i++;
-    }
+    std::shared_ptr<state::Card> card;
+    card = state->drawACard();
+    player->addCard(card);
 }
 
 }
