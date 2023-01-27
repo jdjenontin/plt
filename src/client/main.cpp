c92064641d8bc53469bcf0520eaaa3caf6ddf74f
Uriel DJENONTIN AGOSSOU <jdjenontin@gmail.com>
2023-01-27 21:18:11 +0100

    Last update

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -24,30 +24,6 @@ using namespace ai;
 
 
 
-int netTest() {
-    sf::Http http;
-    http.setHost("http://localhost", 8000);
-
-    sf::Http::Request request;
-    request.setMethod(sf::Http::Request::Post);
-    request.setUri("/");
-    request.setHttpVersion(1, 1);
-    //request.setField("Content-Type", "application/json");
-    request.setBody("{\"name\":\"John\",\"age\":30,\"city\":\"New York\"}");
-
-    sf::Http::Response response = http.sendRequest(request);
-    if (response.getStatus() == sf::Http::Response::Ok) {
-        std::cout << "Response body: " << response.getBody() << std::endl;
-    }
-    else {
-        std::cout << "Error: " << response.getStatus() << std::endl;
-    }
-
-    return 0;
-}
-
-
-
 int main(int argc,char* argv[])
 {
     srand((unsigned) time(NULL));
@@ -108,18 +84,7 @@ int main(int argc,char* argv[])
     }
     else perror("Invaild argument");
 
-    // std::shared_ptr<State> state(new State());
-    // Game game;
 
-    // if(argc == 1){
-    //     game.setState(state);
-    //     game.begin();
-    // }
-    // else if(argc == 2){
-    //     if(strcmp(argv[1], "hello") == 0)
-    //         cout << "Bonjour le monde !" << endl;
-    // }
-    // else perror("Invaild argument");
 
     return 0;
 }
